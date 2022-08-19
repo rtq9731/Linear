cdbbd69dd6eaacb5ac8978a535c30e3d9aabf419
서지호 <103229466+jihoseo2006@users.noreply.github.com>
2022-08-19 19:40:31 +0900

    테스트
    테스트본

---
+++ Lineaer/Assets/02.Scripts/test.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Test", fileName = "testSO")]
+public class test : ScriptableObject
+{
+    public string[] names;
+    public string name = "";
+}
